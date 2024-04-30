e4b22887436d67f020f5ea8bc17ef95bc3892af6
DevSaLLein <isaac.andrade3@aluno.ce.gov.br>
2024-04-30 14:01:41 -0300

    feat: add testes unitários

--- TestesUnitáriosClasse/Program.cs
+++ TestesUnitáriosClasse/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using  TestesUnitáriosClasse.Services;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        CalculadoraService calculadora = new CalculadoraService();
+
+        int num1 = 5;
+        int num2 = 10;
+
+        Console.WriteLine($"{num1} + {num2} = {calculadora.Somar(num1, num2)}");
+    }
+}
